b45f9f1135cbac3bfd16c90759745b3b75d77911
nafeeur <rnafeeur@gmail.com>
2021-10-10 20:38:17 -0400

    add

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -6,9 +6,59 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    @IBOutlet weak var commentField: UITextField!
+    
+    
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        
+
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+                picker.delegate = self
+                picker.allowsEditing = true
+                
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    picker.sourceType = .camera
+                } else {
+                    picker.sourceType = .photoLibrary
+                }
+                present(picker, animated: true, completion: nil)
+            }
+            
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                var image = info[.editedImage] as! UIImage
+                let size = CGSize(width: 300, height: 300)
+                image = image.af_imageAspectScaled(toFill: size)
+                capturedImage.image = image
+                dismiss(animated: true, completion: nil)
+
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
